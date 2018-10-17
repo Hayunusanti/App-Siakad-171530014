af2a5da14840baa33eb4eac9d8a01390556f4f97
Hayunusanti <hayunusantikatrikartikowilis@gmail.com>
2018-10-17 12:41:49 +0700

    Upload

---
+++ dbsiakad_171530014 (1).sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2018 at 07:28 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbsiakad_171530014`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbangkatan`
+--
+
+CREATE TABLE `tbangkatan` (
+  `id_ta` tinyint(4) NOT NULL,
+  `tahun_angkatan` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbjurusan`
+--
+
+CREATE TABLE `tbjurusan` (
+  `kd_jur` char(3) NOT NULL,
+  `jurussan` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbmahasiswa`
+--
+
+CREATE TABLE `tbmahasiswa` (
+  `nim` char(9) NOT NULL,
+  `kd_prodi` char(3) NOT NULL,
+  `id_ta` tinyint(4) NOT NULL,
+  `nama_mhs` varchar(35) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `tmp_lahir` varchar(20) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `agama` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_telepon` varchar(24) NOT NULL,
+  `nama_ayah` varchar(35) NOT NULL,
+  `nama_ibu` varchar(35) NOT NULL,
+  `foto` mediumblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbprodi`
+--
+
+CREATE TABLE `tbprodi` (
+  `kd_prodi` char(3) NOT NULL,
+  `prodi` varchar(45) NOT NULL,
+  `kd_jur` char(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbuser`
+--
+
+CREATE TABLE `tbuser` (
+  `id_user` char(5) NOT NULL,
+  `nama_user` varchar(45) NOT NULL,
+  `pass` varchar(20) NOT NULL,
+  `lev_user` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbuser`
+--
+
+INSERT INTO `tbuser` (`id_user`, `nama_user`, `pass`, `lev_user`) VALUES
+('ID001', 'Code Breaker', 'admin123', 'Administrator');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbangkatan`
+--
+ALTER TABLE `tbangkatan`
+  ADD PRIMARY KEY (`id_ta`);
+
+--
+-- Indexes for table `tbjurusan`
+--
+ALTER TABLE `tbjurusan`
+  ADD PRIMARY KEY (`kd_jur`);
+
+--
+-- Indexes for table `tbmahasiswa`
+--
+ALTER TABLE `tbmahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `tbprodi`
+--
+ALTER TABLE `tbprodi`
+  ADD PRIMARY KEY (`kd_prodi`);
+
+--
+-- Indexes for table `tbuser`
+--
+ALTER TABLE `tbuser`
+  ADD PRIMARY KEY (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
